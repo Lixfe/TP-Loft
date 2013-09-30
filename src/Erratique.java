fc51faec9eacaf63f07c268c0f765a02177bc99b
Lixfe <felix.leo.lecuyer@gmail.com>
2013-09-30 12:03:58 +0200

    Debut constructeur par defaut pour neuneu erratique

--- src/Erratique.java
+++ src/Erratique.java
@@ -1,8 +1,20 @@
+import java.util.*;
+
 public class Erratique extends Neuneu {
 
-  public void Erratique() {
-	  this.nom = "erratique"
-			  this.positionx = 
+	
+	/**
+	 * Constructeur par défaut du Neuneu Erratique : 
+	 * Sa position est generee aleatoirement, il a 20 d'energie.
+	 * 
+	 * Ce constructeur contient l'argument Loft pour donner un lien au Loft.
+	 */
+  public void Erratique(Loft leLoft) {
+	  this.nom = "erratique";
+	  this.positionx = Random.nextInt(10);
+	  this.positiony = Random.nextInt(10);
+	  this.energie = 20;
+	  this.maison = leLoft;
   }
   
   public void sedeplacer(int x, int y){
@@ -21,5 +33,5 @@ public class Erratique extends Neuneu {
 	  
   }
   
-  public
+ 
 }
