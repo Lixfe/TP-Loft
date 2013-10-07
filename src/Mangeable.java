48768d1e66006b286c61be3e9fd9ff209168e7bc
Felix <felix@Green.(none)>
2013-10-07 10:31:50 +0200

    commentaire methode manger neuneu erratique. commentaire methode etremange dans l'interface mangeable

--- src/Mangeable.java
+++ src/Mangeable.java
@@ -2,6 +2,9 @@
  */
 public interface Mangeable {
 
+	/**
+	 * Methode etremange : dans le cas d'une nourriture, met sa quantite a 0, dans le cas d'un neuneu, met son energie a 0.
+	 */
   public void etremange();
 
 }
