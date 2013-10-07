f8f80b8b971141d797fb4362bcdd2d293f5f01ca
RTRaineau <raphael.traineau@gmail.com>
2013-10-07 10:43:31 +0200

    modif dans le main

--- src/TPLoft.java
+++ src/TPLoft.java
@@ -9,8 +9,20 @@ public class TPLoft {
 		
 		//ETAPE2 ajout de nourriture
 		Legume poireau=new Legume();
+		Legume patate=new Legume();
+		Pizza margherita = new Pizza();
 		
-
+		loft.Plateau[1][6].reserve.add(margherita);
+		loft.Plateau[5][9].reserve.add(poireau);
+		loft.Plateau[8][2].reserve.add(patate);
+		
+		//ETAPE3 ajout d'un Neuneu
+		Erratique luc = new Erratique("Luc", loft);
+		
+		//ETAPE4 affichage du loft
+		loft.afficher();
+		
+		
 		
 	
 	}
