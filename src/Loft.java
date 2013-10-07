fe55e6acc016f96725fb2bef8da34fc4d89ef7fb
RTRaineau <raphael.traineau@gmail.com>
2013-10-07 10:00:09 +0200

    constructeur loft par défaut

--- src/Loft.java
+++ src/Loft.java
@@ -3,22 +3,27 @@ import java.util.*;
 public class Loft {
 
   protected LinkedList<Neuneu> Population;
-
-  protected Plateau Cases ;
+  protected Plateau Cases[][] ; //matrice de cases contenant chacune de la nourriture
 
   /**Constructeur du Loft par défaut
    * Crée un Loft de 10*10 cases
    * Il n'y a pas d'habitant
-   * il n'y a pas de nourriture
+   * les cases sont initialisées avec leur constructeur par défaut, donc sont vierge de nourriture
    * 
    */
   public Loft() {
 	  this.Population = new LinkedList<Neuneu>();
-	  this.Plateau = new Cases ;
+	  this.Plateau = new Cases[10][10] ;
+	  for (int i = 0; i < 10; i++) {
+		  for (int j = 0; j < 10; j++) {
+			  this.Plateau[i][j]=Cases();
+			
+		}
+		
+	}
   }
 
-  public void afficher() {
-  }
+  public void afficher() {}  
 
   public void naissance( Neuneu ) {
   }
