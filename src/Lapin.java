a1553d7c8926f1f6a57acd207be5d0f3f6cb9336
unknown <flecuyer@pc-b13-06.eleves.ecn>
2013-10-08 17:08:57 +0200

    amelioration de la strategie de deplacement du neuneu lapin  methode sedeplacer()

--- src/Lapin.java
+++ src/Lapin.java
@@ -35,42 +35,54 @@ public class Lapin extends Neuneu {
   /**
    * Methode sedeplacer() : deplace le neuneu Lapin.
    * 
-   * Le neuneu lapin possède une strategie de deplacement : si un neuneu se trouve sur une case adjacente, il va le rejoindre pour se reproduire avec lui.
+   * Le neuneu lapin possède une strategie de deplacement : il cherche le neuneu le plus proche puis se deplace dans sa direction.
    * 
    */
   public void sedeplacer() {
-	  //on cherche d'abord la presence d'un autre neuneu sur une case adjacente, et on s'y deplace si on en trouve un
-	  boolean pasdeplace = true; // permet de savoir si il y a eu un break
+	  //on parcourt d'abord la liste de neuneus presents sur le plateau pour trouver le plus proche
+	  
+	  //Correspond a la position du neuneu le plus proche dans la liste de neuneu
+	  int index;
+	 index = -1;
+	 //correspond a la distance entre un neuneu et le neuneu lapin considere. On prendra comme definition de distance la distance euclidienne au carre
+	 int distance;
+	 distance = 0;
+	 
+	 //on va chercher le neuneu le plus proche, i.e. ayant la distance minimale tout en etant sur une case differente
 	  for (Neuneu neu : this.maison.Population)
 	  {
-		  if ((neu.positionx <= this.positionx + 1) && (neu.positionx >= this.positionx -1) && (neu.positiony <= this.positiony + 1) && (neu.positiony >= this.positiony -1))
+		  if (distance > (neu.positionx - this.positionx)*(neu.positionx - this.positionx)+(neu.positiony - this.positiony)*(neu.positiony - this.positiony) && neu != this && (neu.positionx - this.positionx)*(neu.positionx - this.positionx)+(neu.positiony - this.positiony)*(neu.positiony - this.positiony)>0)
 		  {
-			  this.positionx = neu.positionx;
-			  this.positiony = neu.positiony;
-			  pasdeplace = false;
-			  break;
-
+			  distance = (neu.positionx - this.positionx)*(neu.positionx - this.positionx)+(neu.positiony - this.positiony)*(neu.positiony - this.positiony);
+			  index = this.maison.Population.indexOf(neu);					  
 		  }
 	  }
 	 
-	  //Si on n'a pas trouve de neuneu a proximite on se deplace de maniere aleatoire
-	 if (pasdeplace)
+	  //Si le neuneu lapin est tout seul dans le loft, il se deplace aleatoirement
+	 if (index == -1)
 	 {
 		  int a,b;
-		  
-		  do
+		  		  do
 		  {
 			  a = (int)((Math.random()*3.)) - 1;	  
 			  b = (int)((Math.random()*3.)) - 1;
-			  /*Nombres aleatoires entre -1 et 1*/
-
-		
+			  /*Nombres aleatoires entre -1 et 1*/		
 		  } while (((this.positionx + a >= this.maison.Plateau[0].length) || (this.positionx + a < 0)) || (this.positiony + b >= this.maison.Plateau.length || this.positiony + b < 0) ||(a==0 && b==0));
 		  
 		  this.positionx = this.positionx+a;
 		  this.positiony = this.positiony+b;
 		  
-
+	 }
+	 else //si on a trouve un neuneu dans le loft on se deplace vers lui
+	 {
+		 if (this.maison.Population.get(index).positionx - this.positionx != 0)
+		 {
+			 this.positionx = (this.maison.Population.get(index).positionx - this.positionx)/Math.abs(this.maison.Population.get(index).positionx - this.positionx);
+		 }
+		 if (this.maison.Population.get(index).positiony - this.positiony != 0)
+		 {
+			 this.positiony = (this.maison.Population.get(index).positiony - this.positiony)/Math.abs(this.maison.Population.get(index).positiony - this.positiony);
+		 }
 	 }
 	 
 	  /* A chaque deplacement on faire perdre 1 point d'energie*/
